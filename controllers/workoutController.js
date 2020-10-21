b9e692b8dd7c6f8a7237aaa3e8c3b9bf3f3543e6
Andrew Stewart <1andrewstewart@gmail.com>
2020-10-21 19:24:57 -0400

    fixed model

--- controllers/workoutController.js
+++ controllers/workoutController.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
-const db = require("../models");
+// const db = require("../models");
 
 router.get("/exercise", (req, res) => {
+  res.json({
+    success: true,
+  });
+})
+
+router.get("/stats", (req, res) => {
   
 })
 
--- models/Workout.js
+++ models/Workout.js
@@ -2,32 +2,40 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-  type: {
-    type: String,
-    trim: true,
-    required: "Type is required",
+  day: {
+    type: Date,
+    default: () => new Date(),
   },
-  name: {
-    type: String,
-    trim: true,
-    required: "Name is required",
-  },
-  duration: {
-    type: Number,
-    required: true,
-  },
-  weight: {
-    type: Number,
-    required: true,
-  },
-  reps: {
-    type: Number,
-    required: true,
-  },
-  sets: {
-    type: Number,
-    required: true,
-  },
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+        required: "Type is required",
+      },
+      name: {
+        type: String,
+        trim: true,
+        required: "Name is required",
+      },
+      duration: {
+        type: Number,
+        required: true,
+      },
+      weight: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+      distance: {
+        type: Number,
+      },
+    },
+  ],
 });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
